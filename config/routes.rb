d8eabd4ebc86851aacc253d6fe9141f9e24e26ba
Dramaclub07 <Priyanshu7ronaldo@gmail.com>
2025-03-11 13:58:44 +0530

    [Priyanshu] UPDATE: Added Cart API routes for managing user carts

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,18 @@ Rails.application.routes.draw do
       post 'forgot_password', to: 'users#forgot_password'
       post 'reset_password', to: 'users#reset_password'
 
-      # Book Routes (Consistent with Authentication Style)
+      # Book Routes
       post 'books/create', to: 'books#create'
       get 'books', to: 'books#index'
       get 'books/:id', to: 'books#show'
       put 'books/:id', to: 'books#update'
-      patch 'books/:id', to: 'books#destroy'
+      delete 'books/:id', to: 'books#destroy'
       patch 'books/:id/is_deleted', to: 'books#is_deleted'
+
+      # Cart Routes
+      post   'cart/add', to: 'carts#add'         
+      patch  'cart/toggle_remove', to: 'carts#toggle_remove' 
+      get    'cart', to: 'carts#index'  
     end
   end
 end
