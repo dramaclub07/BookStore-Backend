5eddf7a45cff70ab95229134b6a02232fe98e1c8
Dramaclub07 <Priyanshu7ronaldo@gmail.com>
2025-03-11 13:58:49 +0530

    [Priyanshu] UPDATE: Modified User factory to support cart-related tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,8 @@
-# spec/factories/users.rb
 FactoryBot.define do
   factory :user do
-    full_name { "John Doe" }
-    email { Faker::Internet.email }
-    password { "password123" }
-    mobile_number { Faker::Number.number(digits: 10) }
+    full_name { Faker::Name.name }
+    email { Faker::Internet.email(domain: %w[gmail.com yahoo.com outlook.com].sample) }
+    password { 'Password@123' }
+    mobile_number { "9#{Faker::Number.number(digits: 9)}" }
   end
 end
