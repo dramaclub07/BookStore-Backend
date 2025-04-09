cb6cbc140d376ae4eddc45b29e491b6b58a0906a
akshay <akshaykatoch38@gmail.com>
2025-04-09 11:53:23 +0530

    [akshay] MOD:Added vercel domain in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # Allow all origins (For development only)
+    origins 'https://book-store-frontend-beige-six.vercel.app/'  # Allow all origins (For development only)
 
     resource '*',
       headers: :any,
