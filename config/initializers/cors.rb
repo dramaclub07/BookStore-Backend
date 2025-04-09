c10ee2000f175ab2133cb6f1d8aec625a7a81816
akshay <akshaykatoch38@gmail.com>
2025-04-09 13:39:03 +0530

    [akshay] MOD:Done few changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://book-store-frontend-beige-six.vercel.app/'  # Allow all origins (For development only)
+    origins 'https://book-store-frontend-beige-six.vercel.app/','http://127.0.0.1:5500'  # Allow all origins (For development only)
 
     resource '*',
       headers: :any,
